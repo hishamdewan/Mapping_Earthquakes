5d6ede9ed673d0cf70bf229e63d6b74e359ecd01
Hisham Dewan <88994059+hishamdewan@users.noreply.github.com>
2021-11-13 09:21:04 -0800

    Added gitignore and API Key

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([36.1733, -120.1794], 7);
 
 //  Add a marker to the map for Los Angeles, California.
 // let marker = L.marker([34.0522, -118.2437]).addTo(map);
@@ -15,6 +15,18 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //     color: "black",
 //     fillcolor: "#ffffa1"
 // }).addTo(map);
+// Coordinates for each point to be used in the line.
+
+// Coordinates for each point to be used in the line.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790]
+  ];
+  
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "red"
+  }).addTo(map);
 
 // Get data from cities.js
 let cityData = cities;
